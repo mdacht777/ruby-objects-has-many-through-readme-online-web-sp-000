1eb3c1eeb0295f9e32a843450a2a05be7fd9d6cb
Martin Achtermann <mdacht777@gmail.com>
2020-06-29 00:26:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,4 +13,8 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
-end
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  endend
